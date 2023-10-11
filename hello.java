e4ad8c4da921f01594ee2fbdef1173f2de0b3bd6
Rocco Vulpis <146395748+roccovulpis@users.noreply.github.com>
2023-10-11 11:03:46 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello
 {
     public static void main ( String argv[])
     {
-        System.out.println( "Hello, CSCI 4485" + argv[0] + "!");
+        System.out.println( "Hello, CSCI 4485!" + argv[0] + "!");
     }
 }
